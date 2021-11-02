bc1a84357f532d94bf050027e22f02d2f6f7c1f0
Viyachikhh <vyachik2k1@gmail.com>
2021-11-02 15:26:05 +0300

    updated core: added possibility to use DenseLayer in __init__ of NeuralNetwork class

--- main.py
+++ main.py
@@ -1,42 +1,52 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-from network.core import categorical_cross_entropy, NeuralNetwork
+from network.core import categorical_cross_entropy, NeuralNetwork, DenseLayer
 from network.load import load
+
+
+def train_model(model, data, labels, epochs=200, batch_size=32, val=None):
+    train_loss = []
+    if val is not None:
+        valid_loss = []
+    ind = np.arange(data.shape[0])
+    for epoch in range(epochs):
+        np.random.shuffle(ind)
+        data = data[ind]
+        labels = labels[ind]
+        print(f'epoch {epoch + 1}')
+        rand_int = np.random.randint(0, X_train.shape[0] - batch_size + 1)
+        pred = model(X_train[rand_int:rand_int + batch_size])
+        loss = categorical_cross_entropy(y_train[rand_int:rand_int + batch_size], pred)
+        network.back_propagation(y_train[rand_int:rand_int + batch_size], pred, batch_size)
+        if val is not None:
+            val_pred = model(val[0])
+            val_loss = categorical_cross_entropy(val[1], val_pred)
+            valid_loss.append(val_loss)
+            print(f'validation loss = {val_loss}', end=', ')
+        print(f'loss = {loss}')
+        train_loss.append(loss)
+    return train_loss, valid_loss if val is not None else train_loss
+
 
 X_train, y_train, X_val, y_val = load()
 
 
 epochs = 200
-batch_size = 256
+batch_size = 512
+
+neural_counts = [512, 256, 128, 256]
+activations = ['relu'] * 4
+#h_1 = DenseLayer(256, 784, 'relu')
+#h_2 = DenseLayer(128, 256, 'relu')
+pre_end = DenseLayer(64, neural_counts[-1], 'relu')
+end_network = DenseLayer(11, 64, 'softmax')
+network = NeuralNetwork(pre_end, end_network, input_shape=X_train.shape[1],
+                        dense_layer_count=4, neural_counts=neural_counts, activations=activations)
+
+train_errors, valid_errors = train_model(network, X_train, y_train, val=(X_val, y_val), batch_size=batch_size)
 
 
-neural_counts = [256, 128, 11]
-activations = ['relu'] * 2 + ['softmax']
-network = NeuralNetwork(dense_layer_count=3, neural_counts=neural_counts, activations=activations)
-train_loss = []
-valid_loss = []
-ind = np.arange(X_train.shape[0])
-for epoch in range(epochs):
-    np.random.shuffle(ind)
-    X_train = X_train[ind]
-    y_train = y_train[ind]
-    print(f'epoch {epoch + 1}')
-    rand_int = np.random.randint(0, X_train.shape[0])
-    #for i in range(0, X_train.shape[0], batch_size):
-    pred = network(X_train[rand_int:rand_int+batch_size])
-    loss = categorical_cross_entropy(y_train[rand_int:rand_int+batch_size], pred)
-    network.back_propagation(y_train[rand_int:rand_int+batch_size], pred, batch_size)
-    #digits_train = np.argmax(pred, axis=1)
-    #digits_real = np.argmax(y_train[rand_int:rand_int+batch_size], axis=1)
-        #print('count of correct predictions = ', round(100 * np.sum(digits_real == digits_train) / batch_size, 3), ' %')
-    val_pred = network(X_val)
-    val_loss = categorical_cross_entropy(y_val, val_pred)
-    print(f'loss = {loss}, val_loss = {val_loss}')
-    train_loss.append(loss)
-    valid_loss.append(val_loss)
-
-plt.plot(train_loss, label='train')
-plt.plot(valid_loss, label='test')
+plt.plot(train_errors, label='train')
+plt.plot(valid_errors, label='test')
 plt.legend()
-plt.savefig('graph.png')
--- network/core.py
+++ network/core.py
@@ -1,15 +1,17 @@
 import numpy as np
 from network.activations import *
 
-
-dict_activations = {'tanh': tanh, 'sigmoid': sigmoid, 'relu': relu, 'softmax': softmax}
-dict_derivatives = {'tanh': derivative_tanh, 'sigmoid': derivative_sigmoid,
+DICT_ACTIVATIONS = {'tanh': tanh, 'sigmoid': sigmoid, 'relu': relu, 'softmax': softmax}
+DICT_DERIVATIVES = {'tanh': derivative_tanh, 'sigmoid': derivative_sigmoid,
                     'relu': derivative_relu, 'softmax': derivative_softmax}
 
 
 class DenseLayer(object):
 
-    def __init__(self, layer_size, prev_layer_size, activation='softmax'):
+    def __init__(self, layer_size, prev_layer_size, activation=None):
+        """
+        initialization of object DenseLayer
+        """
         self.layer_size = layer_size
         self.prev_layer_size = prev_layer_size
         self.weights = np.random.uniform(low=-np.sqrt(6 / (self.layer_size + self.prev_layer_size)),
@@ -17,7 +19,7 @@ class DenseLayer(object):
                                          size=(self.layer_size, self.prev_layer_size))
         self.bias = np.zeros((self.layer_size, 1))
         self.history = (0, 0)
-        self.activation = dict_activations.get(activation, None)
+        self.activation = DICT_ACTIVATIONS.get(activation, None)
 
     def update_weights_and_history(self, gradW, gradb, learning_rate, beta):
         vdw = beta * self.history[0] - learning_rate * gradW
@@ -33,13 +35,17 @@ class DenseLayer(object):
 
 
 class NeuralNetwork(object):
-    def __init__(self, dense_layer_count=2, neural_counts=(128, 11), activations=('relu', 'softmax')):
-        assert dense_layer_count == len(neural_counts)
-        assert dense_layer_count == len(activations)
+    def __init__(self, *your_layers, input_shape=784, dense_layer_count=0,
+                 neural_counts=None, activations=None):
+        if dense_layer_count != 0:
+            assert dense_layer_count == len(neural_counts)
+            assert dense_layer_count == len(activations)
         self.history_outputs = []
         for i in range(dense_layer_count):
             vars(self)[f'layer_{i}'] = DenseLayer(neural_counts[i],
-                                                    784 if i == 0 else neural_counts[i-1], activations[i])
+                                                  input_shape if i == 0 else neural_counts[i - 1], activations[i])
+        for i in range(dense_layer_count, dense_layer_count + len(your_layers)):
+            vars(self)[f'layer_{i}'] = your_layers[i - dense_layer_count]
 
     def __call__(self, inputs):
         self.history_outputs = [inputs]
@@ -58,14 +64,19 @@ class NeuralNetwork(object):
             if i == 0:
                 dZ = y_pred - y_true
             else:
-                weights = getattr(self, layers[i-1]).weights
-                activation = getattr(self, layer).activation.__name__
-                dZ = (dZ @ weights) * dict_derivatives[activation](self.history_outputs[-i])
+                weights = getattr(self, layers[i - 1]).weights
+                layer_activation = getattr(self, layer).activation
+                activation = layer_activation.__name__ if layer_activation is not None else None
+                derivate_act = DICT_DERIVATIVES.get(activation, None)
+                if derivate_act is not None:
+                    derivative_values = derivate_act(self.history_outputs[-i])
+                else:
+                    derivative_values = np.ones(self.history_outputs[-i].shape)
+                dZ = (dZ @ weights) * derivative_values
             db = np.expand_dims((1 / batch_size) * dZ.sum(axis=0), axis=-1)
-            dW = (1 / batch_size) * (np.swapaxes(dZ, 0, 1) @ self.history_outputs[-(i+1)])
+            dW = (1 / batch_size) * (np.swapaxes(dZ, 0, 1) @ self.history_outputs[-(i + 1)])
             getattr(self, layers[i]).update_weights_and_history(dW, db, learning_rate, beta)
 
 
 def categorical_cross_entropy(y_true, y_pred):
     return (-1 / y_true.shape[0]) * np.sum(y_true * np.log(y_pred + 1e-5))
-
