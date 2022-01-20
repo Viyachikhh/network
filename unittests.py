89f87167edde10bba9e2c04c6f6b18b59ca484a1
Viyachikhh <vyachik2k1@gmail.com>
2022-01-20 15:32:38 +0300

    updated unittests

--- unittests.py
+++ unittests.py
@@ -15,13 +15,18 @@ OPT = Momentum(0.9, 0.004)
 class TestRecurrentLayers(unittest.TestCase):
 
     def test_rnn(self):
-        input_shape = (FEATURES, BATCH_SIZE)
+        input_shape1 = (FEATURES, BATCH_SIZE)
+        input_shape2 = (BATCH_SIZE, TIME_STAPS, FEATURES)
         output_shape = (N_UNITS, BATCH_SIZE)
-        inputs = np.random.random(input_shape)
+        inputs1 = np.random.random(input_shape1)
+        inputs2 = np.random.random(input_shape2)
         dZ = np.random.random(output_shape)
         layer = RecLayer(n_units=N_UNITS)
-        self.assertEqual(layer(inputs).shape, output_shape, 'incorrect rnn forward')
-        self.assertEqual(layer.update_weights(dZ, OPT).shape, input_shape, 'incorrect rnn backward')
+        layer2 = RecLayer(n_units=N_UNITS)
+        self.assertEqual(layer(inputs1).shape, output_shape, 'incorrect rnn forward')
+        self.assertEqual(layer.update_weights(dZ, OPT).shape, input_shape1, 'incorrect rnn backward')
+        self.assertEqual(layer2(inputs2).shape, output_shape, 'incorrect rnn timestap forward')
+        self.assertEqual(layer2.update_weights(dZ, OPT).shape, input_shape2, 'incorrect rnn timestap backward')
 
     def test_rnn_time_staps(self):
         input_shape = (BATCH_SIZE, TIME_STAPS, FEATURES)
@@ -33,14 +38,18 @@ class TestRecurrentLayers(unittest.TestCase):
         self.assertEqual(layer.update_weights(dZ, OPT).shape, input_shape, 'incorrect rnn timestap backward')
 
     def test_lstm(self):
-        input_shape = (FEATURES, BATCH_SIZE)
+        input_shape1 = (FEATURES, BATCH_SIZE)
+        input_shape2 = (BATCH_SIZE, TIME_STAPS, FEATURES)
         output_shape = (N_UNITS, BATCH_SIZE)
-        inputs = np.random.random(input_shape)
+        inputs1 = np.random.random(input_shape1)
+        inputs2 = np.random.random(input_shape2)
         dZ = np.random.random(output_shape)
         layer = LSTM(n_units=N_UNITS)
-
-        self.assertEqual(layer(inputs).shape, output_shape, 'incorrect lstm forward')
-        self.assertEqual(layer.update_weights(dZ, OPT).shape, input_shape, 'incorrect lstm backward')
+        layer2 = LSTM(n_units=N_UNITS)
+        self.assertEqual(layer(inputs1).shape, output_shape, 'incorrect lstm forward')
+        self.assertEqual(layer.update_weights(dZ, OPT).shape, input_shape1, 'incorrect lstm backward')
+        self.assertEqual(layer2(inputs2).shape, output_shape, 'incorrect lstm timestap forward')
+        self.assertEqual(layer2.update_weights(dZ, OPT).shape, input_shape2, 'incorrect lstm timestap backward')
 
     def test_lstm_time_staps(self):
         input_shape = (BATCH_SIZE, TIME_STAPS, FEATURES)
@@ -54,4 +63,30 @@ class TestRecurrentLayers(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+"""
 
+case = 0
+
+if case == 0:
+    return_seq = False
+    input_shape = (BATCH_SIZE, TIME_STAPS, FEATURES)
+    output_shape = (N_UNITS, BATCH_SIZE)
+elif case == 1:
+    return_seq = False
+    input_shape = (FEATURES, BATCH_SIZE)
+    output_shape = (N_UNITS, BATCH_SIZE)
+else:
+    return_seq = True
+    input_shape = (BATCH_SIZE, TIME_STAPS, FEATURES)
+    output_shape = (BATCH_SIZE, TIME_STAPS, N_UNITS)
+
+
+inputs = np.random.random(input_shape)
+dZ = np.random.random(output_shape)
+print(inputs.shape, dZ.shape)
+layer = Bidirectional(LSTM(n_units=N_UNITS, return_seq=return_seq))
+pred1 = layer(inputs)
+pred2 = layer.update_weights(dZ, OPT)
+print(pred1.shape, pred2.shape)
+
+"""
